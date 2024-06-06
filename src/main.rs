c6141f859200b8ce7b4f65b3ca4c091625218ad8
Saetch <nils.wriedt@hotmail.com>
2024-06-06 16:11:15 +0200

    refactor name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-#![allow(non_snake_case)]
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
@@ -20,7 +19,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(Snapla::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(snapla::TemplateApp::new(cc))),
     )
 }
 
@@ -37,7 +36,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(Snapla::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(snapla::TemplateApp::new(cc))),
             )
             .await
             .expect("failed to start eframe");
