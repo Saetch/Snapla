8873487c76a9f698ca2f2bcdf761587386aa5227
Saetch <nils.wriedt@hotmail.com>
2024-06-06 16:13:49 +0200

    fmt

--- info_parser_from_docx/src/main.rs
+++ info_parser_from_docx/src/main.rs
@@ -6,10 +6,9 @@ use serde::Serialize;
 
 mod parse;
 
-
 #[derive(Debug)]
 pub(crate) struct ParsedContent {
-    pub(crate) lms : fxhash::FxHashMap<String, LMInformation>,
+    pub(crate) lms: fxhash::FxHashMap<String, LMInformation>,
 }
 
 #[derive(Debug, Serialize)]
@@ -36,16 +35,18 @@ fn main() {
         let file = file.unwrap();
         let file_name = file.file_name();
         let file_name = file_name.to_str().unwrap();
-        println!("{}", file_name);  
-        let parsed_content = read_parse_whacky_input_and_deserialize_docx_file(&format!("information/{}", file_name));
-        
+        println!("{}", file_name);
+        let parsed_content = read_parse_whacky_input_and_deserialize_docx_file(&format!(
+            "information/{}",
+            file_name
+        ));
+
         for (key, value) in &parsed_content.0.lms {
             println!("Lebensmittel: {}", key);
             println!("{}", key.len());
             for (k, v) in value {
-               println!("{}: {}", k, v);
+                println!("{}: {}", k, v);
             }
-    
         }
         parsed_contents.push(parsed_content.0);
         nut_values.push(parsed_content.1);
@@ -65,31 +66,33 @@ fn main() {
     fs::write("serialized/daten.json", json).unwrap();
 }
 
-
 fn combine_info(parsed_contents: Vec<ParsedContent>) -> FxHashMap<String, LMInformation> {
     let mut combined_info = FxHashMap::default();
-    for content in parsed_contents{
+    for content in parsed_contents {
         for (k, value) in content.lms {
             let mut key = k;
-            if key.starts_with("m"){
+            if key.starts_with("m") {
                 key = key[1..].to_string();
             }
-            if key.starts_with("g") && !key.starts_with("ge"){
+            if key.starts_with("g") && !key.starts_with("ge") {
                 key = key[1..].to_string();
             }
-            if key.starts_with("?"){
+            if key.starts_with("?") {
                 key = key[1..].to_string();
             }
-            if key.ends_with("?"){
-                key = key[..key.len()-1].to_string();
+            if key.ends_with("?") {
+                key = key[..key.len() - 1].to_string();
             }
             key = key.replace("Vitamin", "Vitamin ");
-            
-            if combined_info.contains_key(&key){
+
+            if combined_info.contains_key(&key) {
                 let values = &value;
-                combined_info.entry(key).and_modify(|map: &mut FxHashMap<String, f64>| map.extend(values.iter().map(|(k, v)| (k.clone(), *v))));
-            }
-            else {
+                combined_info
+                    .entry(key)
+                    .and_modify(|map: &mut FxHashMap<String, f64>| {
+                        map.extend(values.iter().map(|(k, v)| (k.clone(), *v)))
+                    });
+            } else {
                 combined_info.insert(key, value);
             }
         }
